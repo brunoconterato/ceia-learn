ff118099534a16df692a6d25ee7d3429e9cf5820
Bruno Conterato <brunoconterato@gmail.com>
2022-02-04 15:25:08 -0300

    [FEAT] 3.1 restore checkpoint and other thigs

--- tutorials/ray/3.1-rllib_training_api.py
+++ tutorials/ray/3.1-rllib_training_api.py
@@ -5,6 +5,7 @@ import ray.rllib.agents.ppo as ppo
 ray.init()
 analysis = tune.run(
     "PPO",
+    name="training_api",
     config={
         "env": "CartPole-v0",
         "num_gpus": 1,
@@ -16,3 +17,21 @@ analysis = tune.run(
         "episode_reward_mean": 200,
     }
 )
+
+checkpoints = analysis.get_trial_checkpoints_paths(
+    trial=analysis.get_best_trial("episode_reward_mean"),
+    metric="episode_reward_mean")
+
+last_checkpoint = analysis.get_last_checkpoint()
+last_checkpoint = analysis.get_last_checkpoint(
+    metric="episode_reward_mean", mode="max"
+)
+
+trainer = ppo.PPOTrainer()
+trainer.restore(last_checkpoint)
+
+default_policy_weights = trainer.get_policy().get_weights()
+default_policy_weights = trainer.workers.local_worker().policy_map["default_policy"].get_weights()
+
+list_weights_by_worker = trainer.workers.foreach_worker(lambda w: w.get_policy)
+list_weights_by_worker = trainer.workers.foreach_worker_with_index(lambda w, i: w.get_policy().get_weights())
